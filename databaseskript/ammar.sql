59b2a1e8bef1fb966e3aa5f4aa9cf0340270a5f1
Vikingtor <vikhokjoh@gmail.com>
2021-11-03 02:40:15 +0100

    Commiting changes.

---
+++ databaseskript/ammar.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS Ammar;
+USE Ammar;
+DROP TABLE IF EXISTS Users;
+
+
+CREATE TABLE Users
+(
+    FirstName   VARCHAR(255) NOT NULL,
+    LastName    VARCHAR(255) NOT NULL,
+    Email       VARCHAR(255) NOT NULL,
+    Password    VARCHAR(255) NOT NULL,
+    Admin       BOOLEAN NOT NULL
+);
+
+
+INSERT INTO Users (FirstName, LastName, Email, Password, Admin)
+VALUES ('Viktor', 'Hokland', 'viktor@gmail.com', '123456', true);
+
+INSERT INTO Users (FirstName, LastName, Email, Password, Admin)
+VALUES ('Eivind', 'Halsnes', 'eivind@gmail.com', '123456', false);
+
+INSERT INTO Users (FirstName, LastName, Email, Password, Admin)
+VALUES ('Sebastian', 'Larsen', 'sebastian@gmail.com', '123456', false);
+
+INSERT INTO Users (FirstName, LastName, Email, Password, Admin)
+VALUES ('Ammar', 'Haddad', 'ammar@gmail.com', '123456', false);
